e534e5e7e33883b5912f7bc537568d7e340575e2
szharas <sm.zharas@gmail.com>
2015-03-16 00:06:19 +0600

    horizontal form, buttons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,14 +21,14 @@ angular
 	])
 	.config(function ($routeProvider) {
 		$routeProvider
-			.when('/', {
-				templateUrl: 'views/main.html',
-				controller: 'MainCtrl'
-			})
-			//.when('/about', {
+			// .when('/', {
+			// 	templateUrl: 'views/main.html',
+			// 	controller: 'MainCtrl'
+			// })
+			// .when('/about', {
 			//  templateUrl: 'views/about.html',
 			//  controller: 'AboutCtrl'
-			//})
+			// })
 			.otherwise({
 				redirectTo: '/'
 			});
