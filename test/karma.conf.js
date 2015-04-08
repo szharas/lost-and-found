5fa56c56773019322b66ccbb1d6ec6c7b78f7164
SHYNGGYS ZHARAS <SM.ZHARAS@GMAIL.COM>
2015-04-08 18:52:14 +0600

    message here

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,6 +30,8 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-bootstrap-show-errors/src/showErrors.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/webcomponentsjs/webcomponents.js',
+      'bower_components/polymer/polymer.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
