d6a7e57286fae026f69dd8cc2f34c7900cc2a8c0
izdau <baurzhan@outlook.com>
2015-04-17 12:39:42 +0600

    Changed the select objects (city, category, subCategory) to Angular way. Converted them to dynamic JS variables.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,16 +3,30 @@
 angular.module('lostAndFoundApp')
 	.controller('MainCtrl', function ($scope) {
 		// Initialize variables
+		$scope.cities = ['Астана', 'Алматы'];
+		$scope.lostCategories = ['Документ', 'Портмоне/Сумка', 'Телефон/Планшет', 'Гос. номер', 'Печати/Штампы', 'Другое'];
+		$scope.lostSubCategories = ['Удостоверение личности', 'Паспорт', 'Свидетельство о рождении', 'Водительское удостоверение', 'Свидетельство о регистрации транспортного средства'];
 		$scope.lostAndFound = {};
+		$scope.lostAndFound.city = 'Астана';
 		$scope.lostAndFound.lostCategory = 'Документ';
 		$scope.lostAndFound.lostSubCategory = 'Удостоверение личности';
 		$scope.formType = 'lost';
 		$scope.formTypeRu = 'утери';
 		$scope.isFormOpen = false;
 
+		$scope.selectCity = function (city) {
+			console.log('City selected:' + city);
+			$scope.lostAndFound.city = city;
+		};
 
-		$scope.saveForm = function (lostAndFound) {
-			console.log('Saving object: ' + JSON.stringify(lostAndFound));
+		$scope.selectLostCategory = function (category) {
+			console.log('Category selected: ' + category);
+			$scope.lostAndFound.lostCategory = category;
+		};
+
+		$scope.selectLostSubCategory = function (subCategory) {
+			console.log('Sub category selected: ' + subCategory);
+			$scope.lostAndFound.lostSubCategory = subCategory;
 		};
 
 		$scope.setFormTypeAndToggleOpen = function (formType, formTypeRu) {
@@ -38,5 +52,9 @@ angular.module('lostAndFoundApp')
 
 		$scope.isFoundForm = function () {
 			return ($scope.formType === 'found');
+		};
+
+		$scope.saveForm = function (lostAndFound) {
+			console.log('Saving object: ' + JSON.stringify(lostAndFound));
 		};
 	});
