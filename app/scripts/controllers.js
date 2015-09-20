4c399d4cc7fe11657096c761455a883c427d0a5b
izdau <baurzhan@outlook.com>
2015-09-20 19:16:39 +0600

    committing some changes

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,23 +1,49 @@
 'use strict';
 
 angular.module('lostAndFoundApp')
-	.controller('MainCtrl', function ($scope, $http, toastr) {
+	.controller('MainCtrl', function ($scope, $http, toastr, $mdDialog) {
+		// Declare variables
+		$scope.cities = [
+			'Астана', 'Алматы', 'Актау', 'Актобе', 'Атырау', 'Джезказган', 'Караганда',
+			'Костанай', 'Кокшетау', 'Кызылорда', 'Павлодар', 'Петропавловск', 'Семипалатинск',
+			'Талдыкорган', 'Тараз', 'Туркестан', 'Уральск', 'Усть-Каменогорск', 'Шымкент', 'Экибастуз'
+		];
+		$scope.lostCategories = [
+			'Документ',
+			'Телефон/Планшет',
+			'Гос. номер',
+			'Другое'
+		];
+		$scope.documentSubCategories = ['Удостоверение личности',
+					'Паспорт',
+					'Водительское удостоверение',
+					'Тех. паспорт (Свидетельство о регистрации транспортного средства)',
+					'Свидетельство о рождении',
+					'Диплом',
+					'Военный билет',
+					'Другой документ'
+		];
+
+		$scope.deviceBrands = $http.get('data/deviceBrands.json').success(function(response) {
+			console.log('/data/deviceBrands.json: ' + JSON.stringify(response.data));
+
+			return response.data;
+		});
+
 		// Initialize variables
-		$scope.cities = ['Астана', 'Алматы', 'Актау', 'Актобе', 'Атырау', 'Джезказган', 'Караганда', 'Костанай', 'Кокшетау', 'Кызылорда', 'Павлодар', 'Петропавловск', 'Семипалатинск', 'Талдыкорган', 'Тараз', 'Туркестан', 'Уральск', 'Усть-Каменогорск', 'Шымкент', 'Экибастуз'];
-		$scope.lostCategories = ['Документ', 'Портмоне/Сумка', 'Телефон/Планшет', 'Гос. номер', 'Печати/Штампы', 'Другое'];
-		$scope.lostSubCategories = ['Удостоверение личности', 'Паспорт', 'Свидетельство о рождении', 'Водительское удостоверение', 'Свидетельство о регистрации транспортного средства'];
 		$scope.lostAndFound = {};
-		$scope.lostAndFound.city = 'Астана';
-		$scope.lostAndFound.lostCategory = 'Документ';
-		$scope.lostAndFound.lostSubCategory = 'Удостоверение личности';
+		$scope.city = 'Астана';
 		$scope.lostAndFound.formType = 'lost';
+		$scope.lostAndFound.city ='Астана';
 		$scope.formTypeRu = 'утери';
-		$scope.isFormOpen = false;
+		$scope.isFormOpen = true;
 		$scope.accordionBorderStyle = 'none';
 		$scope.isFormSumbitted = false;
 
+		// Functions
 		$scope.selectCity = function (city) {
 			console.log('City selected:' + city);
+			$scope.city = city;
 			$scope.lostAndFound.city = city;
 		};
 
@@ -26,9 +52,9 @@ angular.module('lostAndFoundApp')
 			$scope.lostAndFound.lostCategory = category;
 		};
 
-		$scope.selectLostSubCategory = function (subCategory) {
-			console.log('Sub category selected: ' + subCategory);
-			$scope.lostAndFound.lostSubCategory = subCategory;
+		$scope.selectDocumentSubCategory = function (documentSubCategory) {
+			console.log('Sub category selected: ' + documentSubCategory);
+			$scope.lostAndFound.documentSubCategory = documentSubCategory;
 		};
 
 		$scope.setFormTypeAndToggleOpen = function (formType, formTypeRu) {
@@ -58,19 +84,31 @@ angular.module('lostAndFoundApp')
 			return ($scope.lostAndFound.formType === 'found');
 		};
 
-		$scope.saveForm = function (lostAndFound, lostAndFoundForm) {
+		$scope.saveForm = function (lostAndFound, lostAndFoundForm, event) {
 			console.log('Saving object: ' + JSON.stringify(lostAndFound));
 			$scope.isFormSumbitted = true;
+			$mdDialog.show(
+				$mdDialog.alert()
+					.parent(angular.element(document.body))
+					.title('Заявка сохранена!')
+					.content('Спасибо Вам за заявку. Пожалуйста сохраните BN20150719')
+					.ariaLabel('Alert Dialog Demo')
+					.ok('OK')
+					.targetEvent(event)
+			);
 
 			if (!lostAndFoundForm.$valid) {
 				toastr.error('Ошибка  в заявке!');
 			} else {
 				$http.post('/buronahodok/lostandfounds/save', lostAndFound).
-					success(function(data, status, headers, config) {
+					success(function (data, status, headers, config) {
+
 						console.log('Server response success: ' + JSON.stringify(data));
+
+
 						toastr.success('Заявка сохранена!');
 					}).
-					error(function(data, status, headers, config) {
+					error(function (data, status, headers, config) {
 						console.log('Server response error: ' + JSON.stringify(data));
 						toastr.error('Ошибка при сохранении!');
 					});
